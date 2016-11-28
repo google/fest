0f824e2ced8fe07d9d9aacc2f851409f0cd3d9cc
Jerome Gaillard <jgaillard@google.com>
2016-11-28 21:21:24 +0000

    Fix AWT.locationOnScreenOf
    Add missing return statement.
    
    Test: Run NlPreviewTest.testCopyAndPaste, though a unit should be added
    Change-Id: Iae841b9286a8bce107a62ed9bc5deeaa1a831a4c

--- third_party/fest-swing/src/main/java/org/fest/swing/awt/AWT.java
+++ third_party/fest-swing/src/main/java/org/fest/swing/awt/AWT.java
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  * 
- * Copyright @2008-2013 the original author or authors.
+ * Copyright @2008-2016 the FEST authors.
  */
 package org.fest.swing.awt;
 
@@ -325,6 +325,7 @@ public class AWT {
     Point parentLocation = locationOnScreenOf(parent);
     if (parentLocation != null) {
       location.translate(parentLocation.x, parentLocation.y);
+      return location;
     }
     return null;
   }
